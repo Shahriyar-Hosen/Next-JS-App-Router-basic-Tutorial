e477d7d27765de8288d4a2c7a397a21767ee219d
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2024-12-21 22:09:11 +0600

    Clean up

--- src/app/(with-nav-footer)/page.tsx
+++ src/app/(with-nav-footer)/page.tsx
@@ -1,12 +1,12 @@
-function getRandomNumber(count: number): number {
-  return Math.floor(Math.random() * count);
-}
+// function getRandomNumber(count: number): number {
+//   return Math.floor(Math.random() * count);
+// }
 
 const HomePage = () => {
-  const showErrorPage = getRandomNumber(2) === 1;
-  if (showErrorPage) {
-    throw new Error("Show Error Page");
-  }
+  // const showErrorPage = getRandomNumber(2) === 1;
+  // if (showErrorPage) {
+  //   throw new Error("Show Error Page");
+  // }
 
   return (
     <main className="flex justify-center items-center w-full h-screen">
