659eae4f252db5dbf670e0ba494194ea748ddb94
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2024-12-22 22:08:39 +0600

    create route and data fetching done

--- src/app/(with-nav-footer)/user-list/page.tsx
+++ src/app/(with-nav-footer)/user-list/page.tsx
@@ -10,14 +10,19 @@ interface IUser {
   role: string;
 }
 
+const getUser = async (): Promise<IUser[]> => {
+  const result = await fetch("http://localhost:3000/users.json").then((res) =>
+    res.json()
+  );
+
+  return result;
+};
+
 const UserList = () => {
   const [users, setUsers] = useState<IUser[]>([]);
 
   useEffect(() => {
-    const getUsers = async () => {
-      return await fetch("/api/users").then((res) => res.json());
-    };
-    getUsers().then((data) => setUsers(data));
+    getUser().then((data) => setUsers(data));
   }, []);
 
   return (
@@ -26,10 +31,10 @@ const UserList = () => {
       <div className="grid grid-cols-5  w-full place-items-center gap-5 flex-wrap pt-20 px-10">
         {users.map(({ name, email, age, id, role }) => (
           <div key={id}>
-            <h1>{name}</h1>
-            <p>{email}</p>
-            <p>{age}</p>
-            <p>{role}</p>
+            <h1>{name} </h1>
+            <p>{email} </p>
+            <p>{age} </p>
+            <p>{role} </p>
           </div>
         ))}
       </div>
